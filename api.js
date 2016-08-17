3e2c96d75544bfe3b9beef1345e2b93c740b3eec
Jordan Fulghum <j.fulghum@gmail.com>
2016-08-17 15:51:34 -0500

    initial commit

---
+++ api.js
@@ -0,0 +1,26 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.lifx.com/v1/lights/all",
+  "method": "GET",
+  "headers": {
+    "authorization": "Bearer c78dd6109b2a0e4aac2aedeaeff660a77be7a8cb12cb20e709bea973f28cb2d0",
+    "cache-control": "no-cache",
+    "postman-token": "8561b10d-baa5-e725-ccee-658fa4f9d492"
+  }
+}
+
+function checkLight() {
+  setTimeout(function() {
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      if (response[0].power == "on") {
+        $("body").css("background-color", "red");
+      } else {
+        $("body").css("background-color", "green");
+      }
+    });
+    checkLight();
+  }, 2000);
+}
+checkLight();
